3ad24c9cc1383b94f81a5460662de967a0ffa756
Craig Rowan <craigmrowan@gmail.com>
2020-09-22 21:09:05 +1200

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 velocity = new Vector2(x, y);
+
+        rigidBody.velocity = velocity * moveSpeed;
+    }
+}
