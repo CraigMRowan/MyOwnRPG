149e7cdb64cd9fd0069dfc91b3c586e51dcc98bd
Craig Rowan <craigmrowan@gmail.com>
2020-10-04 21:34:55 +1300

    Added prototype screen transition system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,55 +1,53 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rigidBody;
-    public float moveSpeed;
-    public int maxHP;
-    private int currentHP;
-    private int currentXP;
-    public int xpToNextLevel;
-    public int attackPower;
-    public int defencePower;
-    private Vector2 facingDirection;
-    public float interactRange;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private int maxHp;
+    [SerializeField] private int xpToNextLevel;
+    [SerializeField] private int attackPower;
+    [SerializeField] private int defencePower;
+    [SerializeField] private float interactRange;
+    
+    
+    private Rigidbody2D _rigidBody;
+    private int _currentHp;
+    private int _currentXp;
+    private Vector2 _facingDirection;
 
-    void Start()
+
+    private void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        currentHP = maxHP;
-        currentXP = 0;
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _currentHp = maxHp;
+        _currentXp = 0;
     }
 
-    void Update()
+    private void Update()
     {
         Move();
         CheckInteract();
     }
 
-    void Move()
+    private void Move()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-        Vector2 velocity = new Vector2(x, y);
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+        var velocity = new Vector2(x, y);
 
         if (velocity.magnitude != 0)
-            facingDirection = velocity;
+            _facingDirection = velocity;
 
-        rigidBody.velocity = velocity * moveSpeed;
+        _rigidBody.velocity = velocity * moveSpeed;
     }
 
-    void CheckInteract()
+    private void CheckInteract()
     {
-        RaycastHit2D cast = Physics2D.Raycast(transform.position, facingDirection, interactRange, 1 << 8);
+        var cast = Physics2D.Raycast(transform.position, _facingDirection, interactRange, 1 << 8);
 
-        if (cast.collider != null)
-        {
-            Interactable interactable = cast.collider.GetComponent<Interactable>();
-
-            if (Input.GetKeyDown(KeyCode.E))
-                interactable.Interact();
-        }
+        if (cast.collider == null) return;
+        var interactable = cast.collider.GetComponent<Interactable>();
+        if (Input.GetKeyDown(KeyCode.E))
+            interactable.Interact();
     }
 }
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Camera.main.transform.Translate(0,10,0);
+        }
+    }
+}
